9f84673e4c577ddd8e76bb339a8c232d9cafe23d
Samuel Cabral <samuelcabral.mail@gmail.com>
2026-05-27 10:56:56 -0300

    feat: add PrismaService and inject into AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common'
+import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   @Get('/hello')
   index(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 
   @Post('/hello')
-  store(): string {
-    return 'teste'
+  async store() {
+    return await this.prisma.user.findMany()
   }
 }
